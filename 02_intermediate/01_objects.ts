13e7f99b10bc0f10ea42c475fe798ea175685d4a
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2024-12-23 21:52:55 +0530

    understanding aliases

--- 02_intermediate/01_objects.ts
+++ 02_intermediate/01_objects.ts
@@ -4,14 +4,26 @@ const User = {
     isActive: true
 }
 
-function createUser({ name: string, isPaid: boolean }) {
+// function createUser({ name: string, isPaid: boolean }) {
 
-}
+// }
 
 let newUser = { name: "Shreya", isPaid: false, email: "email@gmail.com" }
 // weird behavior with parameters
-createUser(newUser);
+// createUser(newUser);
 
 function createCourse(): { name: string, price: number } {
     return { name: "reactjs", price: 399 };
 }
+
+type User = {
+    name: string,
+    email: string,
+    isActive: boolean
+}
+
+function createUser(user: User): User {
+    return { name: "", email: "", isActive: true };
+}
+
+createUser({ name: "", email: "", isActive: true });
