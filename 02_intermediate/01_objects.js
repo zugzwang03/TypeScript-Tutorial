ee6ca0197db20e4fde867ce6fb040ff41f133402
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2024-12-23 21:43:21 +0530

    weird behaviour of objects in ts

---
+++ 02_intermediate/01_objects.js
@@ -0,0 +1,13 @@
+var User = {
+    name: "Shreya",
+    email: "shreya@gmail.com",
+    isActive: true
+};
+function createUser(_a) {
+    var string = _a.name, boolean = _a.isPaid;
+}
+var newUser = { name: "Shreya", isPaid: false, email: "email@gmail.com" };
+createUser(newUser);
+function createCourse() {
+    return { name: "reactjs", price: 399 };
+}
---
+++ 02_intermediate/01_objects.ts
@@ -0,0 +1,17 @@
+const User = {
+    name: "Shreya",
+    email: "shreya@gmail.com",
+    isActive: true
+}
+
+function createUser({ name: string, isPaid: boolean }) {
+
+}
+
+let newUser = { name: "Shreya", isPaid: false, email: "email@gmail.com" }
+// weird behavior with parameters
+createUser(newUser);
+
+function createCourse(): { name: string, price: number } {
+    return { name: "reactjs", price: 399 };
+}
