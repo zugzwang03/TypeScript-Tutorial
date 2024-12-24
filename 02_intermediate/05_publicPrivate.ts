79d3a29a595482599c9a248a3b2e223cb9c28007
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2024-12-24 17:07:23 +0530

    learnt about getters and setters in ts

--- 02_intermediate/05_publicPrivate.ts
+++ 02_intermediate/05_publicPrivate.ts
@@ -9,8 +9,28 @@
 // }
 
 class User {
+    private _courseCount = 1;
     constructor(public email: string, private name: string) {
         this.email = email;
         this.name = name;
+    }
+
+    private deleteToken() {
+        console.log("Token deleted");
+    }
+
+    get getAppleEmail(): string {
+        return `apple${this.email}`;
+    }
+
+    get courseCount(): number {
+        return this._courseCount;
+    }
+
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
     }
 }
