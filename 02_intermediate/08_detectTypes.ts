43e7eda1e2ecd4b0e131a8b6c59b21317f01fbae
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2024-12-24 18:21:42 +0530

    understood about discriminated union

--- 02_intermediate/08_detectTypes.ts
+++ 02_intermediate/08_detectTypes.ts
@@ -42,3 +42,42 @@ function getFood(pet: Fish | Bird) {
         return "bird food";
     }
 }
+
+// discriminated union
+
+interface Circle {
+    kind: "circle",
+    radius: number
+}
+
+interface Square {
+    kind: "square",
+    side: number
+}
+
+interface Rectangle {
+    kind: 'rectangle',
+    length: number,
+    width: number
+}
+
+type Shape = Circle | Square
+
+function getTrueShape(shape: Shape) {
+    if (shape.kind === 'circle') {
+        return Math.PI * shape.radius ** 2;
+    }
+    return shape.side * shape.side;
+}
+
+function getArea(shape: Shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case 'square':
+            return shape.side * shape.side;
+        default:
+            const _defaultForShape: never = shape;
+            return _defaultForShape;
+    }
+}
